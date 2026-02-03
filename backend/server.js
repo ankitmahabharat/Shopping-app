16e1e4da2d70427f1ae7d3272b8adbdde77cf538
Ankit Mahabharat <av700600@gmail.com>
2026-02-03 23:41:53 +0530

    initilizing the files

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+
+
+const app = express();
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
