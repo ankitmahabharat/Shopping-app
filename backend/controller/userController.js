a63d4e5b24882fc754f5a4d127e6b94282a911dc
Ankit Mahabharat <av700600@gmail.com>
2026-02-09 18:05:28 +0530

    added encryption

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,4 +1,5 @@
 import { User } from "../models/userModel.js";
+import bcrypt from "bcryptjs";
 
 export const register = async (req, res) => {
   try {
@@ -10,22 +11,20 @@ export const register = async (req, res) => {
     if (user) {
       return res.status(409).json({ message: "User already exists" });
     }
-    const newUser = await User.create({ firstName, lastName, email, password });
+     const hashPassword = await bcrypt.hash(password,10)
+    const newUser = await User.create({ firstName, lastName, email, password:hashPassword});
     await newUser.save();
-    return res
-      .status(201)
-      .json({
-        success: true,
-        message: "User registered successfully",
-        user: newUser,
-      });
+    return res.status(201).json({
+      success: true,
+      message: "User registered successfully",
+      user: newUser,
+    });
   } catch (error) {
-    return res
-      .status(500)
-      .json({
-        success: false,
-        message: "Internal server error",
-        error: error.message,
-      });
+    // console.log(error);
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+      error: error.message,
+    });
   }
 };
--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,17 @@
-import express from 'express';
-import 'dotenv/config'
-import connectDB from './database/db.js';
-import userRoute from './routes/userRoute.js';
+import express from "express";
+import "dotenv/config";
+import connectDB from "./database/db.js";
+import userRoute from "./routes/userRoute.js";
 
 const app = express();
-const PORT =  process.env.PORT|| 3000;
-
-
-
-
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use('/api/v1/user', userRoute)
-
-
+app.use("/api/v1/user", userRoute);
 
 // http://localhost:8000/api/v1/user/register
 
-
-
-
 app.listen(PORT, () => {
-  connectDB()
-    console.log(`Server is running on http://localhost:${PORT}`);
+  connectDB();
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
