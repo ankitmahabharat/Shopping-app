776242a2b492625b01782e3e820ac7b4bba6a511
Ankit Mahabharat <av700600@gmail.com>
2026-02-10 19:01:51 +0530

    removed env files

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,5 +1,7 @@
 import { User } from "../models/userModel.js";
 import bcrypt from "bcryptjs";
+import jwt from 'jsonwebtoken';
+
 
 export const register = async (req, res) => {
   try {
@@ -13,6 +15,9 @@ export const register = async (req, res) => {
     }
      const hashPassword = await bcrypt.hash(password,10)
     const newUser = await User.create({ firstName, lastName, email, password:hashPassword});
+    const token = jwt.sign({id:newUser._id}, process.env.SECRET_KEY, {expiresIn : '10m'})
+    verifyEmail (token,email)
+    newUser.token= token 
     await newUser.save();
     return res.status(201).json({
       success: true,
---
+++ backend/emailVerify/verifyEmail.js
@@ -0,0 +1,28 @@
+import nodemailer from "nodemailer";
+import "dotenv/config";
+
+const verifyEmail = (token, email) => {
+  let mailTransporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.MAIL_USER,
+      pass: process.env.MAIL_PASS,
+    },
+  });
+
+  let mailDetails = {
+    from: process.env.MAIL_USER,
+    to: email,
+    subject: "Email Verification",
+    text: `Hi! There, You have recently visited our website and entered your email. Please follow the given link to verify your email http ://localhost:5173/verify/${token}Thanks`,
+  };
+
+  mailTransporter.sendMail(mailDetails, function (err, data) {
+    if (err) {
+      console.log("Error Occurs");
+    } else {
+      console.log("Email sent successfully");
+      console.log(data);
+    }
+  });
+};
